64a4040953c2c17a78c27e66897632b5584252f9
Andy De Gheldere <andy.degheldere@gmail.com>
2022-06-28 09:56:54 +0200

    Initial commit

---
+++ src/test/java/com/controlaltinsert/parkshark/ParkSharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlaltinsert.parkshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkSharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
