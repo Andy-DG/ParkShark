f3282e316b272f389fcb7cf9c0cb48ae219ccb45
DracKunn <brecht.vanrie@gmail.com>
2022-06-29 13:53:58 +0200

    path updates

--- src/main/java/com/controlaltinsert/parkshark/division/api/DivisionController.java
+++ src/main/java/com/controlaltinsert/parkshark/division/api/DivisionController.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("division")
+@RequestMapping("divisions")
 @AllArgsConstructor
 public class DivisionController {
 private DivisionService divisionService;
