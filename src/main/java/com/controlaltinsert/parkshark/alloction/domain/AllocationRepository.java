77724e7435a7ca769ad0bb2b8a88480186a197b1
Jurgen Rutten <jurgen.rutten@gmx.com>
2022-07-04 14:32:49 +0200

    no alloc

--- src/main/java/com/controlaltinsert/parkshark/alloction/domain/AllocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.controlaltinsert.parkshark.alloction.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AllocationRepository extends JpaRepository<Allocation, Integer> {
-}
